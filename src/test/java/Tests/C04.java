dc18bbe9028438432ad9871448dbca3022e18460
Ym0000na <m0000nat12@gmail.com>
2025-09-02 17:14:36 +0300

    task 04

--- src/test/java/Tests/C04.java
+++ src/test/java/Tests/C04.java
@@ -1,3 +1,5 @@
+package Tests;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -8,8 +10,21 @@ public class C04 {
     // Go to the w3school URL : https://www.w3schools.com/
         WebDriver driver = new ChromeDriver();
         driver.navigate().to("https://www.w3schools.com/");
-// Print the position and size of the page. 
-// Adjust the position and size of the page as desired. 
-// Test that the page is in the position and size you want. // Close the page.
+
+
+        // Print the position and size of the page
+        System.out.println(driver.manage().window().getPosition());
+        System.out.println(driver.manage().window().getSize());
+
+
+        // Adjust the position and size of the page as desired.
+        driver.manage().window().setPosition(new org.openqa.selenium.Point(200, 200));
+        driver.manage().window().setSize(new org.openqa.selenium.Dimension(600, 600));
+
+
+
+        // Test that the page is in the position and size you want. // Close the page.
+        driver.close();
+        driver.quit();
 }
 }
